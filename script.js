78202f32765e2a665c35de68687e6c81bd0ede4c
sohelhussain <sohelhussaing@gmail.com>
2024-04-04 16:38:43 +0530

    feat:-starting a home animation

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const src = gsap.timeline({
     scrollTrigger:{
         trigger:"#container-third",
         scroller:"body",
-        markers:true,
+        // markers:true,
         start:"0% 0%",
         end:"100% 0%",
         pin:true,
@@ -16,5 +16,50 @@ src
 },"sam")
 .to("#container-third>.third-last",{
     top:"0%",
-
 },"sam")
+
+const homeAni = gsap.timeline()
+
+homeAni.from(".elem-center",{
+    scale: 0
+})
+homeAni.from(".elem-left>.inner-elems>.in-pic",{
+    scale: 0
+}, "home-ani")
+homeAni.from(".elem-right>.inner-elems>.in-pic",{
+    scale: 0
+}, "home-ani")
+
+homeAni.from(".hero-video-box",{
+    y:"-15%",
+    ease: "expo.in"
+    // duration: 2
+})
+
+homeAni.to(".hero-heads",{
+    scale: 1,
+    opacity:1
+})
+
+
+const home = gsap.timeline({
+    scrollTrigger:{
+        trigger:"#container-first",
+        scroller:"body",
+        markers:true,
+        start:"10% 0%",
+        end:"500% 100%",
+        pin: true,
+        scrub: 1
+    }
+})
+home.to(".elem-center",{
+    width:"600%",
+    // height:"300%",
+}, "hom")
+home.to(".elem-left",{
+    x:"-100%"
+}, "hom")
+home.to(".elem-right",{
+    x:"100%"
+}, "hom")
